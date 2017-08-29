8d1e50a14854bcd0a339c5409e9ae2caad7aa4c7
Nguyen Datio <DatioNguyen@outlook.com>
2017-08-29 16:02:08 +1000

    uyt

--- FitMeet/FitMeet/ViewModels/FriendsPageViewModel.cs
+++ FitMeet/FitMeet/ViewModels/FriendsPageViewModel.cs
@@ -191,7 +191,7 @@ namespace FitMeet.ViewModels
             else
             {
                 MessageCount = count.ToString();
-                MessageLogo = "female.png";
+                MessageLogo = "female";
             }
         }
 
